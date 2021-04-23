87f1355db6a4fe5269c55395d9b3ae216fc712c5
milica-bozic <milicabozic1997@gmail.com>
2021-04-23 11:25:08 +0200

    Urađen prvi zadatak!

---
+++ JS_1/script.js
@@ -0,0 +1,16 @@
+// Izračunaj zbir dva broja, a onda razliku neka dva druga broja, a onda od dobijenih
+// rezultata izračunaj proizvod.
+
+let x = 1874;
+let a = 2437;
+let b = 2315;
+let m = 974;
+
+let zbirPrvaDva = x + a;
+console.log("Zbir prva dva broja:", zbirPrvaDva);
+
+let razlikaDrugaDva = b - m;
+console.log("Razlika druga dva broja:", razlikaDrugaDva);
+
+let proizvod = zbirPrvaDva * razlikaDrugaDva;
+console.log("Proizvod zbira i razlike je:", proizvod);
