0092d3e53707dfbe742ac8abc593d429ba7ab544
milica-bozic <milicabozic1997@gmail.com>
2021-04-23 11:26:30 +0200

    Urađen drugi zadatak!

---
+++ JS_2/script.js
@@ -0,0 +1,7 @@
+// Izračunaj proizvod zbira brojeva 1874 i 2437 i razlike brojeva 2315 i 974, tj. (1874 + 2437) * (2315 - 974).
+
+let zbir = (1874 + 2437);
+let razlika = (2315 - 974);
+let proizvod2 = zbir * razlika;
+
+console.log("Proizvod:", proizvod2);
