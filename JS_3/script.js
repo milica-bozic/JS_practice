4b50d457d84391e33491812752106ba6d197173f
milica-bozic <milicabozic1997@gmail.com>
2021-04-23 11:27:26 +0200

    Urađen treći zadatak!

---
+++ JS_3/script.js
@@ -0,0 +1,7 @@
+// Dužina stranice kvadrata je 4. Izračunajte njegovu površinu i obim.
+
+let k = 4;
+let obim = k * 4;
+let povrsina = k ** 2
+console.log("Obim kvadrata dužine 4 cm je:", obim, "cm.");
+console.log("Površina kvadrata čija je dužine stranice 4 cm je:", povrsina, "cm.");
