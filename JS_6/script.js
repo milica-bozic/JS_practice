574caff1369ff52aa0b7cfc9fc456b4a80c762ac
milica-bozic <milicabozic1997@gmail.com>
2021-04-24 12:39:28 +0200

    Urađen 6. zadatak!

---
+++ JS_6/script.js
@@ -0,0 +1,15 @@
+// 6. zadatak
+// Za dva uneta broja ispisati koji je veći a koji je manji.
+
+let a = 1511;
+let b = 151;
+
+if (a > b) {
+    console.log(`a = ${a} je veci broj, b = ${b} je manji broj`);
+}
+else if (a == b) {
+    console.log(`a = ${a} je jednako sa b = ${b}.`);
+}
+else {
+    console.log(`a = ${a} je manji broj, b = ${b} je veci broj`);
+}
