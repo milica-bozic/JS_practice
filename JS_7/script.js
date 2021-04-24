b57b9e106666c75208460fe36a1c3e87af08570b
milica-bozic <milicabozic1997@gmail.com>
2021-04-24 12:41:15 +0200

    Urađen 7. zadatak!

---
+++ JS_7/script.js
@@ -0,0 +1,12 @@
+console.log("Hello Mika");
+// 7. Zadatak 
+/* Ispitati da li je uneta temperatura u plusu ili je
+u minusu. Ukoliko je temperatura nula, računati da je u plusu. */
+
+let temperatura = 0;
+if (temperatura >= 0) {
+    console.log(`Temperatura, koja je ${temperatura} je u plusu.`);
+}
+else {
+    console.log(`Temperatura, koja je ${temperatura} je u minusu.`);
+}
