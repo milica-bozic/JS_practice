87f52df6765c52bac0a2ec7d48df0604859e5a63
milica-bozic <milicabozic1997@gmail.com>
2021-04-24 12:37:43 +0200

    Urađen 4. zadatak!

---
+++ JS_4/script.js
@@ -0,0 +1,9 @@
+/* Pera je zasadio 380 stabala jabuke. Đura je zasadio 142 stabla jabuka više od Pere, 
+   a Mika je zasadio dva puta više od Pere. Koliko su stabala zasadili zajedno? */
+
+   let pera = 380;
+   let djura = pera + 142;
+   let mika = pera * 2;
+   let zajedno = pera + djura + mika;
+
+   console.log("Zajedno su zasadili:", zajedno, "stabala.");
