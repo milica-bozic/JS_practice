395b2f0146abf15284936d1062d9dc6cc11eed95
milica-bozic <milicabozic1997@gmail.com>
2021-04-25 11:14:49 +0200

    Urađen 11. zadatak!

---
+++ JS_11/script.js
@@ -0,0 +1,15 @@
+//11. Zadatak
+// Odrediti najveći od tri uneta broja.
+
+let a = -566;
+let b = 5863;
+let c = 100;
+
+let maks = a;
+ if (b > maks) {
+    maks=b;
+ }
+ if (c > maks) {
+    maks = c;
+ }
+ console.log(maks);
