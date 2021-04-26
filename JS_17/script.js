37584c81c5b93e29b8e521f2bcfc9d2a107abddc
milica-bozic <milicabozic1997@gmail.com>
2021-04-26 21:25:20 +0200

    Urađen 17. zadatak!

---
+++ JS_17/script.js
@@ -0,0 +1,11 @@
+// Ispisati brojeve od 1 do 20.
+// Svaki neka bude ispisan u istom redu.
+
+let i = 1;
+let rezultat = "";
+
+while (i <= 20) {
+    rezultat = rezultat + i + " ";
+    i++;
+}
+console.log(rezultat);
