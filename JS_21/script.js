e82f3f976340dfd3f6e89ec1b8ef1a21a8d13963
milica-bozic <milicabozic1997@gmail.com>
2021-04-27 12:09:56 +0200

    Urađen 21. zadatak!

---
+++ JS_21/script.js
@@ -0,0 +1,20 @@
+// Ispisati brojeve koji su deljivi sa 3 do 87.
+let i = 1;
+
+while (i <= 87) {
+    if (i % 3 == 0) {
+        console.log(i);
+    }
+    i++
+}
+
+// Ispisati brojeve deljive sa 9.
+
+i = 1;
+
+while (i <= 99) {
+    if (i % 9 == 0) {
+        console.log(i)
+    }
+    i++
+}
