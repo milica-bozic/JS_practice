f188666a4b22b75afb79f0a36388efad6776cf73
milica-bozic <milicabozic1997@gmail.com>
2021-04-28 22:05:43 +0200

    Urađen 28. zadatak!

---
+++ JS_28/script.js
@@ -0,0 +1,15 @@
+console.log("Ispisati brojeve od 1 do 100.");
+
+let i = 1;
+let n = 100;
+
+for (i = 1; i <= n; i++) {
+    console.log(i);
+}
+
+console.log("Ispisati brojeve od 100 do 1.");
+
+i = 100;
+for (i = 100; i >= 1; i--) {
+    console.log(i);
+}
