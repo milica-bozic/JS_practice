533aa312300d25d6c13d6697cc0e5d174edb9917
milica-bozic <milicabozic1997@gmail.com>
2021-04-28 22:06:26 +0200

    Urađen 29. zadatak!

---
+++ JS_29/script.js
@@ -0,0 +1,27 @@
+console.log("1. Ispisati parne brojeve od 1 do 20.");
+let i = 1;
+for (i = 1; i <= 20; i++) {
+    if (i % 2 == 0) {
+        console.log(i);
+    }
+}
+
+console.log("2. Ispisati neparne brojeve od 1 do 20");
+i = 1;
+for (i = 1; i <= 20; i += 2) {
+        console.log(i);
+}
+
+console.log("3. Ispisati parne brojeve od 1 do 100.");
+i = 1;
+for (i = 1; i <= 100; i++) {
+    if (i % 2 == 0) {
+        console.log(i);
+    }
+}
+
+console.log("4. Ispisati neparne brojeve od 1 do 100");
+i = 1;
+for (i = 1; i <= 100; i += 2) {
+        console.log(i);
+}
