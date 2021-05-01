e00d1b9a73e97fc80e0c8d315c9cb29d6cb3036c
milica-bozic <milicabozic1997@gmail.com>
2021-05-01 23:51:13 +0200

    Urađen 41. zadatak!

---
+++ JS_41/script.js
@@ -0,0 +1,7 @@
+let num1 = window.prompt("Enter your first number");
+let num2 = window.prompt("Enter your second number");
+
+num1 = parseFloat(num1);
+num2 = parseFloat(num2);
+
+document.write(num1 + num2);
