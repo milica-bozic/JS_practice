3d03bc21511f6ded1c92ad983474407e80f747a3
milica-bozic <milicabozic1997@gmail.com>
2021-05-01 23:05:14 +0200

    Urađen 37. zadatak!

---
+++ JS_37/script.js
@@ -0,0 +1,4 @@
+let name = window.prompt("What is your name?");
+let age = window.prompt("How old are you?");
+
+document.write("Hey " + name + ", how are you today?" + " Do you like being " + age + " years old?");
