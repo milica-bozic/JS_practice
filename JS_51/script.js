10d8070a34b98d0e954e370c9ca77055c5920a7f
milica-bozic <milicabozic1997@gmail.com>
2021-05-13 16:11:45 +0200

    Urađen 51. zadatak!

---
+++ JS_51/script.js
@@ -0,0 +1,10 @@
+// 2. zadatak
+// Odrediti zbir elemenata celobrojnog niza.
+// celobrojni - niz koji se sastoji od celih brojeva
+let brojevi = [150, 140, 360, 120, 180];
+let suma = 0;
+
+for (i = 0; i < brojevi.length; i++) {
+    suma += brojevi[i];
+}
+console.log(suma);
