1a2744c55853685de37cc3a23dcc1635269758cb
milica-bozic <milicabozic1997@gmail.com>
2021-05-15 23:04:08 +0200

    Urađen 71. zadatak!

---
+++ JS_71/script.js
@@ -0,0 +1,7 @@
+// 19. zadatak
+// Ispisati dužinu svakog elementa u nizu stringova.
+let imena = ['Marko', 'Žarko', 'Milica', 'Milivoje', 'Ljubica'];
+
+for (let i = 0; i < imena.length; i++) {
+    console.log(imena[i].length);
+}
