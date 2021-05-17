2509eea8a9f16658f9595e7e42a09b56f4ebdb20
milica-bozic <milicabozic1997@gmail.com>
2021-05-17 15:02:03 +0200

    Urađen 76.zadatak!

---
+++ JS_76/script.js
@@ -0,0 +1,26 @@
+function mojaFunkcija() {
+    console.log("Zdaravo svete!");
+}
+
+mojaFunkcija();
+mojaFunkcija();
+mojaFunkcija();
+mojaFunkcija();
+
+
+function mojaDrugaFunkcija() {
+    document.body.innerHTML += `<h3>Zdravo svete!</h3>`;
+}
+
+mojaDrugaFunkcija();
+mojaDrugaFunkcija();
+
+
+function moja_funkcija(tekst) {
+    console.log(tekst);
+}
+
+moja_funkcija("ITBootcamp!");
+
+let tekst = 'Zdravo Milice!';
+moja_funkcija(tekst);
