163a090883f02c42809b6d61be3b6bc1e3014050
milica-bozic <milicabozic1997@gmail.com>
2021-05-17 15:26:08 +0200

    Urađen 77. zadatak!

---
+++ JS_77/script.js
@@ -0,0 +1,18 @@
+// Napisati funkciju zbir1 koja ispisuje zbir svoja dva parametra.
+
+function zbir1(a, b) {
+    let rezultat = a + b;
+    console.log(rezultat);
+}
+
+
+zbir1(2, 3); 
+zbir1(4,10);
+
+let a = 4;
+let b = 6;
+zbir1(a, b);
+
+a = 10;
+b = 11;
+zbir1( a, b);
