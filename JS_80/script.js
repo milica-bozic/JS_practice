dfb07366d3dfe7ceb1b41e177b7d1c5b581e394c
milica-bozic <milicabozic1997@gmail.com>
2021-05-17 16:22:21 +0200

    Urađen 80. zadatak!

---
+++ JS_80/script.js
@@ -0,0 +1,13 @@
+// Izracunati proizvod dva parametra prko funkcije.
+function proizvod(a, b) {
+    return a * b;
+}
+
+console.log(proizvod(2, 3));
+console.log(proizvod(4, 5));
+console.log(proizvod(7, 6));
+console.log(proizvod(10, 10));
+console.log(proizvod(5, 6));
+console.log(proizvod(2, 67));
+console.log(proizvod(9, 5));
+console.log(proizvod(4, 4));
