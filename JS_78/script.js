f89e2d74d7181c58bcec0cfc048b944af044ffaa
milica-bozic <milicabozic1997@gmail.com>
2021-05-17 16:20:54 +0200

    Urađen 78. zadatak!

---
+++ JS_78/script.js
@@ -0,0 +1,9 @@
+// Funkcija koja vraca svoja dva parametra
+function zbir(a, b) {
+    let rezultat = a + b;
+    return rezultat;
+}
+
+let z = zbir(4, 6);
+console.log(`Zbir brojeva 4 i 6 jednak je: ${z}`);
+console.log(`Sum of numbers 4 and 6 is: ${z}`);
