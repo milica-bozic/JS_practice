3ab9ebe0689562e0d66769b1b2c7646c6b49c920
milica-bozic <milicabozic1997@gmail.com>
2021-05-17 16:23:21 +0200

    Urađen 81. zadatak!

---
+++ JS_81/script.js
@@ -0,0 +1,10 @@
+// 1. zadatak:
+/*
+Napisati funkciju pozdrav kojoj se prosleđuje ime i prezime, a funkcija ispisuje pozdrav.
+Na primer za uneto ime Jelena i prezime Matejić, funkcija ispisuje Zdravo Jelena Matejić.
+*/
+
+function pozdrav(ime, prezime) {
+    console.log(`Pozdrav ${ime} ${prezime}!`);
+}
+pozdrav("Jelena", "Matejic");
