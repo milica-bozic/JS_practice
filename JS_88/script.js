933f9e4737e5ab068d9e7e78f0403547d9ad6c06
milica-bozic <milicabozic1997@gmail.com>
2021-05-18 16:14:51 +0200

    Urađen 88. vežbanje!

---
+++ JS_88/script.js
@@ -0,0 +1,17 @@
+// 25. zadatak
+/* Dati su nizovi 
+a[0], a[1], ...,a[n - 1] i
+b[0], b[1], ...,b[n - 1].
+formirati niz c[0], c[1], ...,c[n - 1] ciji su elementi
+a[0] * b[0], a[1] * b[1], ...,a[n - 1] * b[n - 1]
+*/
+
+let zad25 = (a, b) => {
+    let n = a.length;
+    let c = [];
+    
+    for (let i = 0; i < 0; i++) {
+        c[i] = a[i] * b[i];
+    }
+    return c;
+}
