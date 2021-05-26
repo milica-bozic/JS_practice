0739c443d616e6c7876918e822634a75d1f7d76a
milica-bozic <milicabozic1997@gmail.com>
2021-05-26 22:37:09 +0200

    Urađeno vežbanje 140!

---
+++ JS_140/script.js
@@ -0,0 +1,30 @@
+// SVOJSTVA DOCUMENT OBJEKATA:
+
+// String:
+console.log(document.URL);
+console.log(typeof document.URL);
+
+///////////////////////////////////////////////
+// Objekat iz DOM stabla:
+console.log(document.documentElement);
+console.log(typeof document.documentElement);
+
+console.log(document);
+console.log(typeof document);
+
+console.log(document.body);
+console.log(typeof document.body);
+
+//////////////////////////////////////////////////
+// HTMLCollection:
+console.log(document.images);
+console.log(typeof document.images);
+
+console.log(document.anchors);
+console.log(typeof document.anchors);
+
+console.log(document.links);
+console.log(typeof document.links);
+
+console.log(document.forms);
+console.log(typeof document.forms);
