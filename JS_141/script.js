b7613b80d958d3de9581ed8bbb0765767e25e815
milica-bozic <milicabozic1997@gmail.com>
2021-05-26 23:19:54 +0200

    Urađeno vežbanje 141!

---
+++ JS_141/script.js
@@ -0,0 +1,9 @@
+console.log(document.getElementById("para"));
+let paragraf = document.getElementById("maneskin");
+console.log(paragraf);
+
+let paragrafi1 = document.getElementsByClassName("small");
+console.log(paragrafi1);
+
+let paragrafi3 = document.getElementsByTagName("p");
+console.log(paragrafi3);
